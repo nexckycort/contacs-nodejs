0163c72affde2f87278ae3eac18b868b290b3e84
Nestor Cortina <junior110120@gmail.com>
2020-11-16 02:21:19 -0500

    agregando variables de entorno y morgan

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,8 +1,11 @@
 import express, { Application, Request, Response } from 'express';
+import morgan from 'morgan';
 
 import routesV1 from './routes/v1';
 
 const app: Application = express();
+
+app.use(morgan('dev'));
 
 app.use('/', routesV1);
 
