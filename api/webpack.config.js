94b3cad7f5ff45f9540219c567339e925c63bae8
Nestor Cortina <junior110120@gmail.com>
2020-11-16 01:56:46 -0500

    configurando webpack

--- api/webpack.config.js
+++ api/webpack.config.js
@@ -12,21 +12,5 @@ module.exports = {
     filename: 'server.js'
   },
   target: 'node',
-  node: {
-    __dirname: false,
-    __filename: false
-  },
-  externals: [nodeExternals()],
-  target: 'node',
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader'
-        }
-      }
-    ]
-  }
+  externals: [nodeExternals()]
 }
