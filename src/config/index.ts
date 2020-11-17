3d900b825f08a63b2d1587ebef99ab903f05809a
Nestor Cortina <junior110120@gmail.com>
2020-11-16 22:27:42 -0500

    prefix api

--- src/config/index.ts
+++ src/config/index.ts
@@ -4,7 +4,6 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 const envFound = dotenv.config();
 if (envFound.error) {
-  // This error should crash whole process
   throw new Error("⚠️  Couldn't find .env file  ⚠️");
 }
 
@@ -12,8 +11,8 @@ export default {
   nameAPI: process.env.NAME_API,
   port: process.env.PORT || 9000,
   databaseURL: process.env.MONGODB_URI,
-  jwtSecret: process.env.JWT_SECRET,
+  jwtSecret: process.env.JWT_SECRET || '',
   api: {
-    prefix: '/api',
+    prefix: '/v1',
   }
 };
--- src/loaders/express.ts
+++ src/loaders/express.ts
@@ -14,7 +14,7 @@ app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
 app.use(morgan('dev'));
 
-app.use('/v1', routesV1);
+app.use(config.api.prefix, routesV1);
 
 // catch 404 and forward to error handler
 app.use((_req: Request, res: Response, next: NextFunction) => next(NotFoundError(res)));
